b0fe0aa0e43db4e37dd50ce42f91d7f2d5326b9c
Raunak Bajaj <53440377+raunakb007@users.noreply.github.com>
2021-04-10 10:01:47 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+
+def add_ints(a, b):
+    return a + b
+
+
+def subtract_ints(a, b):
+    return a - b
+
+
+def multiply_ints(a, b):
+    return a * b
+
+
+def divide_ints(a, b):
+    return a / b
+
+def is_greater_than(num, target):
+    if num > target:
+        return True
+    else:
+        return False
